ba21f648959d16f66842326f6892556ca36b893a
Malcolm Kiano <malcolmkiano@gmail.com>
2023-09-04 19:01:49 -0400

    fix: handle existing input values

--- decimal-input.js
+++ decimal-input.js
@@ -1,4 +1,4 @@
-function decimalInput(options = false){
+function decimalInput(options = false) {
   // set the options if not set
   if (!options) options = {};
   // provide for any missing values
@@ -7,8 +7,10 @@ function decimalInput(options = false){
   options.validation = options.validation || "soft";
   // check if length was provided, and if it is a number
   if (options.length) {
-    if (isNaN(options.length) || (options.length < 1)){
-      console.warn("decimal-input: Invalid length given: '" + options.length + "'")
+    if (isNaN(options.length) || options.length < 1) {
+      console.warn(
+        "decimal-input: Invalid length given: '" + options.length + "'"
+      );
       options.length = 2;
     }
   } else {
@@ -17,22 +19,28 @@ function decimalInput(options = false){
 
   // run
   var inputs = document.querySelectorAll(options.selector);
-  inputs.forEach(function(input){
+  inputs.forEach(function (input) {
     // sets the pattern for the input to allow
     input.setAttribute("pattern", "\\d*");
 
     // checks if input-level length is set, and is valid
-    if (input.dataset.length){
-      if (isNaN(input.dataset.length) || (input.dataset.length < 1)){
-        console.warn("decimal-input: Invalid length given: '" + input.dataset.length + "'")
+    if (input.dataset.length) {
+      if (isNaN(input.dataset.length) || input.dataset.length < 1) {
+        console.warn(
+          "decimal-input: Invalid length given: '" + input.dataset.length + "'"
+        );
         input.dataset.length = 2;
       }
     }
-    var length = input.dataset.length ? parseInt(input.dataset.length) : options.length;
+    var length = input.dataset.length
+      ? parseInt(input.dataset.length)
+      : options.length;
     var mid = ".";
     var end = "";
-    var isHeight = input.dataset.format == "height" || (!input.dataset.format && options.format == "height");
-    if (isHeight){
+    var isHeight =
+      input.dataset.format == "height" ||
+      (!input.dataset.format && options.format == "height");
+    if (isHeight) {
       // set data-type to text to allow quote marks
       input.setAttribute("type", "text");
       input.setAttribute("inputmode", "numeric");
@@ -43,24 +51,25 @@ function decimalInput(options = false){
     }
 
     // set value for input (uses default value if set)
-    if (!input.dataset.defaultValue){
-      for (var i = 0; i < length + 1; i++){
+    if (!input.value && !input.dataset.defaultValue) {
+      for (var i = 0; i < length + 1; i++) {
         input.value += "0";
       }
       input.dataset.content = "";
     } else {
-      input.value = input.dataset.defaultValue;
-      input.dataset.content = input.value;
+      const initialValue = input.value || input.dataset.defaultValue;
+      input.value = initialValue;
+      input.dataset.content = initialValue.replace(/[\.'"]/g, "");
     }
 
     // add delimiter
-    function formatInput(content = true){
+    function formatInput(content = true) {
       var value = input.value.toString();
-      if (content){
+      if (content) {
         var c = input.dataset.content;
-        if (c.length < length + 1){
+        if (c.length < length + 1) {
           var pre = "";
-          for (var i = 0; i < length + 1 - c.length; i++){
+          for (var i = 0; i < length + 1 - c.length; i++) {
             pre += "0";
           }
           c = pre + c;
@@ -68,23 +77,25 @@ function decimalInput(options = false){
         value = c.toString();
       }
 
-      var before = value.substring(0, value.length - length);
+      var before = value
+        .substring(0, value.length - length)
+        .replace(/[\.'"]/g, "");
       var after = value.substring(value.length - length);
 
       // validation for height, autocorrect to correct feet and inches value
-      if (isHeight){
+      if (isHeight) {
         if (before > 0) {
           // if validation is hard
-          if (options.validation == "hard"){
-            while (after > 11){
+          if (options.validation == "hard") {
+            while (after > 11) {
               before = parseInt(before) + 1;
               after = parseInt(after) - 12;
-              if (after == 0){
-                after = after.toString() + "0";
+              if (after == 0) {
+                after = `${after}0`;
               } else if (after < 10) {
-                after = "0" + after.toString();
+                after = `0${after}`;
               }
-              input.dataset.content = before.toString() + after.toString();
+              input.dataset.content = `${before}${after}`;
               formatInput(true);
             }
           } else {
@@ -96,17 +107,17 @@ function decimalInput(options = false){
         }
       }
       // delimit value
-      input.value = before + mid + after + end;
+      input.value = `${before}${mid}${after}${end}`;
     }
 
     // bind input function to format
-    input.onkeydown = function(e){
+    input.onkeydown = function (e) {
       // remove invalid marker if present
       input.removeAttribute("invalid");
       var key = e.key;
-      if (e.key == "Backspace"){
+      if (e.key == "Backspace") {
         var content = input.dataset.content;
-        if (content){
+        if (content) {
           input.dataset.content = content.substring(0, content.length - 1);
         }
       } else if (isNaN(key)) {
@@ -117,7 +128,7 @@ function decimalInput(options = false){
       formatInput();
 
       return false;
-    }
+    };
 
     // initial format
     formatInput(false);
